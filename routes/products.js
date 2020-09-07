3f9a454f1f78d55f344002c31c3b349848622475
Karthik <46497070+BobbyDazzlar@users.noreply.github.com>
2020-09-07 13:21:49 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -55,4 +55,16 @@ router.get('/search', authenticateJWT, (req, res) => {
         }
     });
 });
+router.get('/get', authenticateJWT, (req, res) => {
+    req.body = url.parse(req.url, true).query;
+    localPool.query('SELECT * FROM products  ORDER BY RAND() limit ? OFFSET ?', [`%${req.body.search}%`, req.body.sortBy, req.body.dir, req.body.pageSize, req.body.page * req.body.pageSize], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.json(results);
+            res.status(200).end();
+        }
+    });
+});
 module.exports = router;
