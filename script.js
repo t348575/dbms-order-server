bd796d897ca03e786af73b571fd696c30222a3b0
Joseph Subash Kanichai <40432493+t348575@users.noreply.github.com>
2020-09-07 23:53:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ function randomNumbers(max) {
         return myArr.shift();
     };
 }
+
+Array.prototype.max = function(comparer) {
+
+    if (this.length === 0) return null;
+    if (this.length === 1) return this[0];
+
+    comparer = (comparer || Math.max);
+
+    var v = this[0];
+    for (var i = 1; i < this.length; i++) {
+        v = comparer(this[i], v);
+    }
+
+    return v;
+}
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 (async() => {
     /*let randoms = randomNumbers(1000),
     rand = randoms(),
@@ -75,6 +95,69 @@ function randomNumbers(max) {
     const parser = new Parser(opts);
     const csv2 = parser.parse(prod_all);
     fs.writeFileSync(path.join(__dirname, '/csv/total.csv'), csv2);*/
-    const flipkart = await csv().fromFile(path.join(__dirname, '/csv/flipkart_com-ecommerce_sample.csv'));
-    console.log(flipkart.length);
+    let flipkart = await csv().fromFile(path.join(__dirname, '/csv/flipkart_com-ecommerce_sample.csv'));
+    let randoms = randomNumbers(5000),
+        rand = randoms(),
+        result = [];
+    while(rand != null) {
+        result.push(rand);
+        rand = randoms();
+    }
+    const sub = 20000 - result.max();
+    result = result.map(a => a + sub);
+    let newFlipkart = [];
+    for(const [idx, v] of flipkart.entries()) {
+        delete flipkart[idx].uniq_id;
+        delete flipkart[idx].crawl_timestamp;
+        delete flipkart[idx].product_url;
+        delete flipkart[idx].pid;
+        delete flipkart[idx].discounted_price;
+        delete flipkart[idx].is_FK_Advantage_product;
+        delete flipkart[idx].product_category_tree;
+        delete flipkart[idx].brand;
+        delete flipkart[idx].overall_rating;
+        try {
+            flipkart[idx].product_specifications = flipkart[idx].product_specifications.replace(/=>/gi, ':');
+            flipkart[idx].product_specifications = (JSON.parse(flipkart[idx].product_specifications)).product_specification;
+            const newSpec = {};
+            for (const v of flipkart[idx].product_specifications) {
+                if (v.hasOwnProperty('key') && v.hasOwnProperty('value')) {
+                    newSpec[v.key] = v.value;
+                }
+            }
+            flipkart[idx].product_specifications = newSpec;
+        } catch (e) {
+            flipkart[idx].product_specifications = [];
+        }
+        try {
+            const arr = JSON.parse(flipkart[idx].image);
+            let link = arr[0];
+            for (const v of arr) {
+                if (v.indexOf('/g/') === -1) {
+                    link = v;
+                    break;
+                }
+            }
+            flipkart[idx].image = link;
+        } catch (e) {
+            flipkart[idx].image = '';
+        }
+        newFlipkart.push({
+            prod_id: crypto.randomBytes(32).toString('hex'),
+            prod_name: flipkart[idx].product_name,
+            prod_desc: flipkart[idx].description,
+            prod_img: flipkart[idx].image,
+            prod_rating: (flipkart[idx].product_rating === 'No rating available' ? -1 : flipkart[idx].product_rating),
+            prod_price: parseInt(flipkart[idx].retail_price, 10),
+            prod_feat: JSON.stringify(flipkart[idx].product_specifications),
+            prod_dim: (result.indexOf(idx) > -1 ? getRandomInt(1, 100) : JSON.stringify({ x: getRandomInt(1, 1000), y: getRandomInt(1, 1000), z: getRandomInt(1, 1000), w: getRandomInt(1, 100) })),
+            prod_type: (result.indexOf(idx) > -1 ? 1 : 0),
+            prod_stock: getRandomInt(0, 1000)
+        });
+    }
+    const fields = ['prod_id', 'prod_name', 'prod_desc', 'prod_img', 'prod_feat', 'prod_dim', 'prod_type', 'prod_price', 'prod_stock', 'prod_rating'];
+    const opts = { fields };
+    const parser = new Parser(opts);
+    const csv2 = parser.parse(newFlipkart);
+    fs.writeFileSync(path.join(__dirname, '/csv/parsed_flipkart.csv'), csv2);
 })();
