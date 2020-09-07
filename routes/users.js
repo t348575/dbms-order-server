830a31ecf1a21dd6d74732730f5dd0c84f227409
Joseph Subash Kanichai <40432493+t348575@users.noreply.github.com>
2020-09-07 12:08:25 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const key = fs.readFileSync(path.join(__dirname, '/../private.cert'));
+const publicKey = fs.readFileSync(path.join(__dirname, '/../public.cert'));
 const localPool = mysql.createPool({
     connectionLimit: 1000000000,
     host: 'localhost',
@@ -20,51 +21,91 @@ function getDate(date) {
     const newDate = new Date(date);
     return newDate.getFullYear() + '-' + (newDate.getMonth() + 1) + '-' + newDate.getDate();
 }
-router.post('/login', (req, res) => {
-    localPool.query('SELECT cust_email, cust_passwd FROM customer WHERE cust_email=? OR cust_phone=?', [req.body.username, req.body.username],(err, result) => {
+function login(req, res) {
+    jwt.sign({
+        data: { username: req.body.username },
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + 3600
+    }, key, { algorithm: 'RS256' }, function(err1, jwtToken) {
+        if (err1) {
+            console.log(err1);
+            res.json({ status: false, apiToken: '', loginToken: '' });
+            res.status(200).end();
+        } else {
+            const loginToken = crypto.randomBytes(32).toString('hex');
+            localPool.query('UPDATE customer SET cust_token = ? WHERE cust_email = ?', [loginToken, req.body.username], (err2) => {
+                if (err2) {
+                    res.json({ status: false, apiToken: '', loginToken: '' });
+                    console.log(err2);
+                } else {
+                    res.json({ status: true, apiToken: jwtToken, loginToken });
+                }
+                res.status(200).end();
+            });
+        }
+    });
+}
+router.post('/autoLogin', (req, res, next) => {
+    jwt.verify(req.body.apiToken, publicKey, { ignoreExpiration: true },(err, data) => {
+        req.tokenData = data;
+        req.body.username = data.data.username;
+        if (req.body.username === undefined || req.body.username === null) {
+            res.json({ status: false, apiToken: '', loginToken: '' });
+            res.status(200).end();
+        } else {
+            localPool.query('SELECT * FROM customer WHERE cust_token = ? AND cust_email = ?', [req.body.loginToken, req.body.username], (err) => {
+                if (err) {
+                    console.log(err);
+                    res.json({ status: false, apiToken: '', loginToken: '' });
+                    res.status(200).end();
+                } else {
+                    next();
+                }
+            });
+        }
+    });
+}, login);
+router.post('/login', (req, res, next) => {
+    console.log(req.body);
+    localPool.query('SELECT cust_email, cust_passwd FROM customer WHERE cust_email = ? OR cust_phone = ?', [req.body.username, req.body.username],(err, result) => {
         if (err) {
             console.log(err);
-            res.json({ status: false, token: '' });
+            res.json({ status: false, apiToken: '', loginToken: '' });
+            res.status(200).end();
         } else if (result.length === 1) {
             bcrypt.compare(req.body.password, result[0].cust_passwd, function(err, hashResults) {
                 if (err) {
                     console.log(err);
-                    res.json({ status: false, token: '' });
+                    res.json({ status: false, apiToken: '', loginToken: '' });
                     res.status(200).end();
                 } else {
-                    jwt.sign({
-                        data: { username: result[0].cust_email },
-                        iat: Math.floor(Date.now() / 1000) - 30
-                    }, key, { algorithm: 'RS256' }, function(err1, jwtToken) {
-                        if (err1) {
-                            console.log(err1);
-                            res.json({ status: false, token: '' });
-                        } else {
-                            res.json({ status: true, token: jwtToken });
-                        }
+                    if (hashResults) {
+                        next();
+                    } else {
+                        res.json({ status: false, apiToken: '', loginToken: '' });
                         res.status(200).end();
-                    });
+                    }
                 }
             });
         } else {
-            res.json({ status: false, token: '' });
+            res.json({ status: false, apiToken: '', loginToken: '' });
             res.status(200).end();
         }
     });
-});
+}, login);
 router.post('/register', (req, res) => {
     bcrypt.hash(req.body.password, 10, function(err, hash) {
         if (err) {
             console.log(err);
-            res.json({ status: false });
+            res.json({ status: false, message: 'Improper password format!' });
             res.status(200).end();
         } else {
-            localPool.query('INSERT INTO customer VALUES(?,?,?,?,?,?,?)', [crypto.randomBytes(32).toString('hex'), req.body.email, hash, req.body.name, getDate(req.body.dob), req.body.phone, req.body.address], (err) => {
+            localPool.query('INSERT INTO customer VALUES(?,?,?,?,?,?,?,?)', [crypto.randomBytes(32).toString('hex'), req.body.email, hash, '',req.body.name, req.body.dob, req.body.phone, req.body.address], (err) => {
                 if (err) {
                     console.log(err);
-                    res.json({ status: false });
+                    res.json({ status: false, message: 'Account under this email or phone already exists!' });
                 } else {
-                    res.json({ status: true });
+                    res.json({ status: true, message: 'Account creation successful!' });
                 }
                 res.status(200).end();
             });
