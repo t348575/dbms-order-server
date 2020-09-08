ee0b68599093e9e243a3e596333086cf2eab4757
Karthik <46497070+BobbyDazzlar@users.noreply.github.com>
2020-09-08 12:48:55 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -69,4 +69,19 @@ router.get('/browse', (req, res) => {
         }
     });
 });
+router.get('/predictSearch', (req, res) => {
+    req.body = url.parse(req.url, true).query;
+    req.body.page = parseInt(req.body.page, 10);
+    req.body.pageSize = parseInt(req.body.pageSize, 10);
+    localPool.query('SELECT * FROM products  WHERE  prod_name LIKE ? or PROD_NAME LIKE ? OR prod_name LIKE ? OR prod_name LIKE ? LIMIT 5;', [req.body.search, `${req.body.search}%`, `%${req.body.search}`, `%${req.body.search}%`], (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.json(results);
+            res.status(200).end();
+        }
+    });
+});
+
 module.exports = router;
